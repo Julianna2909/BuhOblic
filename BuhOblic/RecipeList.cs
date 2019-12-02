6429ee18f67c14f74bdac420533003e8cca9481d
Julianna2909 <julianna29kalinich@gmail.com>
2019-12-02 22:59:05 +0200

    Added DataBase

--- BuhOblic/RecipeList.cs
+++ BuhOblic/RecipeList.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace BuhOblic
 {
-    public partial class Form1 : Form
+    public partial class RecipeList : Form
     {
-        public Form1()
+        public RecipeList()
         {
             InitializeComponent();
         }
